cc529870e63de7276bcae6029f6ab8e1fe58d229
P4ndaM1x <77618102+P4ndaM1x@users.noreply.github.com>
2023-10-08 18:10:38 +0200

    fixed jupyter server token added

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,3 @@
+docker build /mnt/d/studies/inżynierka/code/docker -t tensorflowongpu
 docker run -it -v /mnt/d/studies/inżynierka/:/host -p 8888:8888 --gpus all tensorflowongpu
 docker exec -it 4dc3a310de8b bash

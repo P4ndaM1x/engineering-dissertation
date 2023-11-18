47fd384c6665a9900ed569aafffb5f8039f851ed
P4ndaM1x <77618102+P4ndaM1x@users.noreply.github.com>
2023-11-18 21:28:46 +0100

    phi_estimation refactored

--- common/model.py
+++ common/model.py
@@ -1,7 +1,10 @@
 import tensorflow.math as tf_math
+
 from tensorflow import convert_to_tensor
 from tensorflow.keras.losses import Loss
 class AngleError(Loss):
+    def __init__(self):
+        super().__init__(name='angle_error')
     def call(self, y_true, y_pred):
         y_pred = convert_to_tensor(y_pred)
         y_true = convert_to_tensor(y_true)
@@ -10,6 +13,10 @@ class AngleError(Loss):
 from tensorflow import Tensor
 def convert_to_real_with_angle(z: Tensor) -> Tensor:
     return tf_math.angle(z)
+
+def add_custom_objects_to_keras():
+    from tensorflow.keras.utils import get_custom_objects
+    get_custom_objects().update({'angle_error': AngleError, 'convert_to_real_with_angle': convert_to_real_with_angle})
 
 import gc
 from tensorflow.keras.backend import clear_session

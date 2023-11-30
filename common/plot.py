0a306480f59c2d5e7a63bf5f78eec415992f3489
P4ndaM1x <77618102+P4ndaM1x@users.noreply.github.com>
2023-11-30 23:14:19 +0100

    prepared q_classification plots

--- common/plot.py
+++ common/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from common.utils import make_dataframe, moving_average
+from common.utils import make_dataframe, moving_average, validation_set, train_set
 
 def plot_metric_history(fit_history, metric = 'loss', moving_average_window = (10,), title = None):
     
@@ -29,7 +29,7 @@ def plot_metric_history(fit_history, metric = 'loss', moving_average_window = (1
     plt.show()
 
 
-def plot_metric_history_zoomed(fit_history, metric = 'loss', mode = 'min', max = 1, min = 0, moving_average_window = (10,), threshold_multiplier = 10, legend_loc = 'lower left', title = None):
+def plot_metric_history_zoomed(fit_history, metric = 'loss', mode = 'min', max = 1, min = 0, moving_average_window = (10,), threshold_multiplier = 10, subplots=3, legend_loc = 'lower left', title = None, dpi=100):
 
     if isinstance(fit_history, pd.core.frame.DataFrame):
         df = fit_history
@@ -48,26 +48,32 @@ def plot_metric_history_zoomed(fit_history, metric = 'loss', mode = 'min', max =
         print(f'99th percentile of train {metric}:       {train_99th_percentile:.4e}')
         print(f'99th percentile of validation {metric}:  {val_99th_percentile:.4e}')
         threshold = np.minimum(train_99th_percentile, val_99th_percentile)
-        
-    fig, axes = plt.subplots(3, sharex=True, figsize=(8,6))
-    if title is None:
-        axes[0].set_title(f'{metric} history')
+    
+    if subplots < 3:
+        fig, axes = plt.subplots(2, sharex=True, figsize=(8,6),  gridspec_kw={'height_ratios': [1, 2]}, constrained_layout=True)
     else:
+        fig, axes = plt.subplots(subplots, sharex=True, figsize=(8,6), constrained_layout=True)
+    fig.set_dpi(dpi)
+    if title is not None:
+        # axes[0].set_title(f'{metric} history')
+    # else:
         axes[0].set_title(title)
         
     for axis in axes:
+        # axis.grid(axis='x', linestyle='--', linewidth=0.5)
         axis.plot(df['epoch'], df[metric], label='training set')
         axis.plot(df['epoch'], df[f'val_{metric}'], linestyle='dotted', linewidth=0.5, label='validation set')
         if moving_average_window is not None:
             for w in moving_average_window:
-                axis.plot(df['epoch'][w-1:], moving_average(df[f'val_{metric}'], w), linestyle='dashed', label=f'validation set {metric} moving average, w={w}')
+                axis.plot(df['epoch'][w-1:], moving_average(df[f'val_{metric}'], w), linestyle=(0, (1, 1)), label=f'validation set - moving average, $w={w}$')
         if mode == 'min':
             axis.set_ylim((min, threshold_multiplier*threshold))
             threshold_multiplier /= 2
         else:
             axis.set_ylim((threshold_multiplier*threshold, max))
-            threshold_multiplier /= 1.1
-    axes[-1].legend(loc=legend_loc)
+            threshold_multiplier /= 1.2
+    if legend_loc:
+        axes[-1].legend(loc=legend_loc)
     axes[-1].set_xlabel("epoch no.")
     fig.supylabel(f"{metric} value")
 
@@ -90,35 +96,54 @@ def plot_loss_by_parameter(loss, parameter, loss_label='', parameter_label='', s
     plt.legend()
 
 
-def plot_steps_by_parameter(x_data, y_data, parameter, metric_func, metric_label='', parameter_label='', bins=25, ylim=None, filter=lambda data: data):
+def plot_steps_by_parameter(model, x_data, y_data, parameter, metric_func, metric_label='', parameter_label='', bins=25, ylim=None, show_train=True, show_val=True, dpi=100):
     
     import tensorflow.keras.utils as tf_utils
     tf_utils.disable_interactive_logging()
     
-    x_data=filter(x_data)
-    y_data=filter(y_data)
-    parameter=filter(parameter)
+    x_data_train=train_set(x_data)
+    y_data_train=train_set(y_data)
+    parameter_train=train_set(parameter)
+    
+    x_data_val=validation_set(x_data)
+    y_data_val=validation_set(y_data)
+    parameter_val=validation_set(parameter)
 
-    _, bin_edges = np.histogram(parameter, bins=bins)
-    bars = []
+
+    _, bin_edges_train = np.histogram(parameter_train, bins=bins)
+    _, bin_edges_val = np.histogram(parameter_val, bins=bins)
+    bars_train = []
+    bars_val = []
     for i in range(bins):
-        indexes = np.where(np.logical_and(parameter>=bin_edges[i], parameter<=bin_edges[i+1]))
-        bars.append( float(metric_func(np.asarray(y_data)[indexes], model.predict(x_data[indexes]))) )
+        indexes = np.where(np.logical_and(parameter_train>=bin_edges_train[i], parameter_train<=bin_edges_train[i+1]))
+        bars_train.append( float(metric_func(np.asarray(y_data_train)[indexes], model.predict(x_data_train[indexes]))) )
+        indexes = np.where(np.logical_and(parameter_val>=bin_edges_val[i], parameter_val<=bin_edges_val[i+1]))
+        bars_val.append( float(metric_func(np.asarray(y_data_val)[indexes], model.predict(x_data_val[indexes]))) )
 
-    plt.figure()
+    plt.figure(dpi=dpi)
     # plt.hlines(bars, bin_edges[:-1], bin_edges[1:], colors='red', lw=4, label=f'binned')
     # plt.bar(bin_edges[:-1], bars, width=bin_edges[1]-bin_edges[0], align='edge')
-    bars.insert(0, bars[0])
-    plt.step(bin_edges, bars, where='pre', color='red')
-    plt.fill_between(bin_edges, bars, step="pre", alpha=0.5)
-    offset = 0.001
+    
+    if show_train:
+        bars_train.insert(0, bars_train[0])
+        plt.step(bin_edges_train, bars_train, where='pre', label='training set')
+        plt.fill_between(bin_edges_train, bars_train, step="pre", alpha=0.4)
+    
+    if show_val:
+        bars_val.insert(0, bars_val[0])
+        plt.step(bin_edges_val, bars_val, where='pre', ls=(0, (5, 3)), label='validation set')
+        plt.fill_between(bin_edges_val, bars_val, step="pre", alpha=0.4)
+    
     plt.xlabel(parameter_label)
     plt.ylabel(metric_label)
     plt.grid(linestyle='--', linewidth=0.5)
+    offset = 0.001
     if ylim:
         plt.ylim(ylim)
     else:
-        plt.ylim(((1-offset)*np.min(bars), (1+offset)*np.max(bars)))
+        plt.ylim(((1-offset)*np.min(bars_train), (1+offset)*np.max(bars_train)))
+    if show_train and show_val:
+        plt.legend()
     plt.show()
     
     tf_utils.enable_interactive_logging()
